bb4209c46811f7059a5715f9040cb57c1381ff6a
Denasia <dfurniss@ymail.com>
2023-03-15 23:12:07 -0400

    pretty much started over, lots of comments, some old code is commented out to be reimplemented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { useState } from "react";
-import "./App.css";
 import RecipeData from "./RecipeData";
-import RecipeCreate from "./RecipeCreate";
+// import RecipeCreate from "./RecipeCreate";
 import RecipeList from "./RecipeList";
+import "./styles/App.css";
 
-// TODO: Add the ability for the <RecipeList /> component to list and delete an existing recipe.
-// TODO: Add the ability for the <RecipeCreate /> component to create new recipes.
+/*
+TODO: recipe is a card, card is clicked and goes to different full page, recipe page
+  card is just the image with name underneath.. carasol style ? 
+TODO: favorite a recipe and have a separte section to list the favorited recipes
+
+? use bootstrap??
+? mobile first development??
+
+* Random features I want to implement in the long run:
+  - be able to search for a recipe by name, cuisine, and ingredients; search bar at top
+  - in each strip, load the first 25 recipes then when they get to the end, have a 'show all' option that opens another page or just expands all the recipes in that category down
+ */
 
 export default function App() {
 //---state declaration---
@@ -23,7 +33,9 @@ export default function App() {
     <div className="App">
       <header><h1>Delicious Food Recipes</h1></header>
       <RecipeList recipes={recipes} deleteRecipe={deleteRecipe}/>
-      <RecipeCreate addRecipe={addRecipe}/>
+        {/* favorite recipes */}
+      {/* <RecipeList recipes={recipes} deleteRecipe={deleteRecipe}/> */}
+      {/* <RecipeCreate addRecipe={addRecipe}/> */}
     </div>
   );
 };
--- src/RecipeList.js
+++ src/RecipeList.js
@@ -1,5 +1,6 @@
 import React from "react";
-import OneRecipe from "./OneRecipe";
+import OneRecipe from "./OneRecipeBlip";
+import {AiFillCaretRight} from "react-icons/ai"
 
 // TODO: Display the list of recipes using the structure of table that is provided.
 // TODO: Create at least one additional component that is used by this component.
@@ -7,25 +8,32 @@ import OneRecipe from "./OneRecipe";
 //Each recipe passed as a prop to OneReipe will return as a tr to fill in the tbody
 
 export default function RecipeList({recipes, deleteRecipe}) {
+  
   return (
-    <div className="recipe-list">
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Cuisine</th>
-            <th>Photo</th>
-            <th>Ingredients</th>
-            <th>Preparation</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-            {recipes.map((recipe, index)=> 
-            <OneRecipe key={index} recipe={recipe} deleteRecipe={()=>deleteRecipe(index)}/>)}
-        </tbody>
-      </table>
+    <div className="recipe-list-div">
+      <div className="category-name">
+        <p>All Recipes</p>
+      </div>
+        <div className="row">
+          <div className="recipe-list-carasol">
+            {recipes.length && recipes.map((recipe, idx) => <OneRecipe recipe={recipe} deleteRecipe={deleteRecipe} key={idx}/>)}
+          </div>
+          <div id="transp"></div>
+          <AiFillCaretRight
+            id="right-arrow"
+            size={30}
+            onClick={(e)=>{
+              // console.log(e.target.parentElement.children[0].scrollLeft)
+              e.target.parentElement.parentElement.children[0].scrollLeft += 270 * 5;
+              // console.log(window.innerWidth)
+                  // one blip client width is 270
+              // ^ at 1500 and above, innerWidth - scroll by 5 blips
+              // below 1500 scroll by 4s
+              // below 1220, scroll by 3s
+              // below 950 scroll by 2s
+              // below 680 scroll by 1
+              }}/>
+        </div>
     </div>
   );
 };
-
